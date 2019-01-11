97230a7ccec5db4b9d81433dfbb9800a99599d30
Cobus <hoffmann.cobus@hotmail.com>
2019-01-11 13:13:18 +1000

    Started on serial interface to master programmer

--- config tool/WRD-MasterProgrammer/main.c
+++ config tool/WRD-MasterProgrammer/main.c
@@ -1,18 +1,35 @@
 /*
  * WRD-MasterProgrammer.c
  *
- * Created: 1/11/2019 11:20:41 AM
+ * Created: 1/11/2019 12:12:51 PM
  * Author : Cobus Hoffmann
  */ 
 
 #include <avr/io.h>
+#include "../../library/nrf24l01.h"
+#include "../../library/serial.h"
 
 
 int main(void)
 {
     /* Replace with your application code */
+	serialInit(9600);
+	unsigned char myBuffer[20];
+	volatile unsigned char myFlag = 0;
+	
+	//Set buffer and flag for the serial stuff
+	serialSetRxBuffer(myBuffer);
+	serialSetRxCompleteFlag(&myFlag);
+	//Enable the reception via interrupt
+	serialEnableInterrupt();
     while (1) 
     {
+		if (myFlag)
+		{
+			//Serial data has been received
+			serialPrintLn("Received:");
+			serialPrintLn(myBuffer);
+		}
     }
 }
 
