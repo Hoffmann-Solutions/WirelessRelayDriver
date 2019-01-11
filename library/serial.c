574989dc17464621d7684468d06896eb01925fa2
Cobus <hoffmann.cobus@hotmail.com>
2019-01-11 13:14:52 +1000

    Serial files added to library

---
+++ library/serial.c
@@ -0,0 +1,8 @@
+/*
+ * serial.c
+ *
+ * Created: 1/11/2019 12:16:48 PM
+ *  Author: Cobus Hoffmann
+ */ 
+
+#include "serial.h"
---
+++ library/serial.h
@@ -0,0 +1,18 @@
+/*
+ * serial.h
+ *
+ * Created: 1/11/2019 12:16:37 PM
+ *  Author: Cobus Hoffmann
+ */ 
+
+
+#ifndef SERIAL_H_
+#define SERIAL_H_
+
+#include <avr/io.h>
+
+
+
+
+
+#endif /* SERIAL_H_ */
